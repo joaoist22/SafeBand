a03b98a20ef6322e451cd4a563e05274a7120428
João Sousa <joaofsovsa@gmail.com>
2026-03-18 17:24:05 +0000

    feat: Adicionado Gantt chart, correção da navbar e luzes do diamante

--- src/data/posts.js
+++ src/data/posts.js
@@ -7,7 +7,8 @@ import fotoJuan from '../assets/equipa/JL.jpeg';
 import fotoFalardo from '../assets/equipa/JF.jpeg';
 import fotoValentim from '../assets/equipa/VM.jpeg';
 import fotoJesus from '../assets/equipa/Jesus.jpeg';
-import snoopyImg from '../assets/fotos_diversas/snoopy.jpg'; 
+import logo from '../assets/logo/logo_dif.png';
+import imgreuniao from '../assets/fotos_diversas/reuniao.jpeg';
 
 export const postData = [
   {
@@ -23,7 +24,7 @@ export const postData = [
     authorLinkedIn: "https://www.linkedin.com/in/joaof-sousa",
     
     date: "1 Março, 2026",
-    image: snoopyImg,
+    image: logo,
     excerpt: "Grandes eventos enfrentam lacunas de segurança críticas que a tecnologia atual não resolve.",
     content: `A sobrecarga da rede móvel é um cenário-padrão em grandes aglomerações. 
     Isto impede que os participantes peçam ajuda e que as equipas de socorro se coordenem.
@@ -31,8 +32,8 @@ export const postData = [
   },
    {
     id: "3",
-    tag: "rrrrr",
-    title: "Vida",
+    tag: "reunião",
+    title: "Reunião de Planeamento de comunicação",
     author: "João Sousa",
     
     // 2. Usar a variável da foto (Sem aspas!)
@@ -41,12 +42,10 @@ export const postData = [
     // 3. O Link que vai abrir quando clicarem no teu nome/foto
     authorLinkedIn: "https://www.linkedin.com/in/joaof-sousa",
     
-    date: "1 Março, 2026",
-    image: snoopyImg,
-    excerpt: "Grandes eventos enfrentam lacunas de segurança críticas que a tecnologia atual não resolve.",
-    content: `A sobrecarga da rede móvel é um cenário-padrão em grandes aglomerações. 
-    Isto impede que os participantes peçam ajuda e que as equipas de socorro se coordenem.
-    A SafeBand resolve isto através de uma plataforma integrada.` 
-  }
+    date: "15 Março, 2026",
+    image: imgreuniao,
+    excerpt:"A comunicação é um dos pilares fundamentais para o sucesso de qualquer projeto. Esta reunião de planeamento foi crucial para alinhar a equipa e definir as estratégias e ações tecnológicas necessárias para superar os desafios de conectividade em eventos de grande escala.",
+    content:`A sobrecarga das redes móveis é um cenário recorrente em grandes aglomerações. Nesta reunião, focámo-nos no planeamento da comunicação do projeto, discutindo as melhores formas de manter a equipa informada e tecnicamente alinhada.Tivemos a oportunidade de aprofundar a visão da equipa e debater como implementar estas soluções num evento como a Santa Sebenta (AEIST), analisando a viabilidade da escala para contextos ainda maiores. Entre as ideias discutidas, destacaram-se o uso de pulseiras como recetores passivos de BLE, permitindo a propagação de mensagens em rede, e a utilização de tecnologia LoRa para comunicações de emergência com baixa latência. Embora seja apenas o início de um longo caminho, este foi um passo decisivo para garantir a coesão da equipa e a viabilidade técnica do projeto.` 
+}
 
 ];
